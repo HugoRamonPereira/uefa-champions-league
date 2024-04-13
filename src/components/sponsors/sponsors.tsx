2ea9ac35cb0a2575e21cc7a0b86bc8fc9f7c4eb4
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-04-13 01:06:27 -0300

    Global sponsors component

---
+++ src/components/sponsors/sponsors.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Sponsors() {
+  return (
+    <section className="bg-[url('/assets/images/ucl-global-sponsors.jpg')] bg-cover bg-center h-40 w-full">
+      <div className="w-full max-w-[700px] flex flex-col items-center h-full justify-center gap-4 mx-auto text-sm antialiased font-light tracking-wide pt-10">
+        <p>Official global sponsors</p>
+        <div className="flex gap-4">
+          <Link href="https://www.heineken.com/" target="_blank">
+            <Image src="/assets/images/heineken.webp" alt="Heineken" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.playstation.com/" target="_blank">
+            <Image src="/assets/images/ps5.avif" alt="Play Station Five" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.lays.com/" target="_blank">
+            <Image src="/assets/images/lays.webp" alt="Lays" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.fedex.com/" target="_blank">
+            <Image src="/assets/images/fedex.webp" alt="FedEx" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.mastercard.com/" target="_blank">
+            <Image src="/assets/images/mastercard.webp" alt="Mastercard" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.takeaway.com/" target="_blank">
+            <Image src="/assets/images/jet-house.webp" alt="Jet house" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.oppo.com/" target="_blank">
+            <Image src="/assets/images/oppo.webp" alt="Oppo" width={140} height={77} quality={100} />
+          </Link>
+          <Link href="https://www.turkishairlines.com/" target="_blank">
+            <Image src="/assets/images/turkish-airlines.webp" alt="Turkish Airlines" width={140} height={77} quality={100} />
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
