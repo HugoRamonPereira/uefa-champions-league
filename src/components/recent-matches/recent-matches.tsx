27e2fa0d57365b21c34c2dad1f4a57a4cbd1a9b6
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-04-14 23:40:57 -0300

    Added responsiveness for cell phone size

--- src/components/recent-matches/recent-matches.tsx
+++ src/components/recent-matches/recent-matches.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import FootballField from "../../../public/assets/icons/football-field";
 import MatchCard from "../match-card/match-card";
 import { Button } from "../ui/button";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+
 import realMadrid from "../../../public/assets/icons/clubs/real-madrid.png";
 import manCity from "../../../public/assets/icons/clubs/man-city.png";
 import arsenal from "../../../public/assets/icons/clubs/arsenal.png";
@@ -13,8 +19,8 @@ import bvb from "../../../public/assets/icons/clubs/bvb.png";
 export default function RecentMatches() {
   return (
     <section className="h-32 bg-uefa-dark-blue-main flex w-full">
-      <div className="flex items-center justify-between w-full max-w-[1400px] mx-auto">
-        <div className="ml-16 flex gap-4">
+      <div className="flex items-center justify-between w-full max-w-[1400px] mx-auto overflow-x-hidden">
+        <div className="hidden sm:flex gap-4 ml-16">
           <MatchCard
             teamOne="Real Madrid"
             teamOneImage={realMadrid}
@@ -56,7 +62,7 @@ export default function RecentMatches() {
             teamTwoScore={1}
           />
         </div>
-        <div>
+        <div className="hidden sm:flex justify-end">
           <Button variant="uefa-cyan-outline" className="flex items-center gap-2 group">
             <FootballField className="fill-uefa-cyan group-hover:fill-uefa-dark-blue-main rotate-90" />
             View all matches
--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -3,34 +3,38 @@ import Link from "next/link";
 
 export default function Sponsors() {
   return (
-    <section className="bg-[url('/assets/images/ucl-global-sponsors.jpg')] bg-cover bg-center h-40 w-full">
-      <div className="w-full max-w-[700px] flex flex-col items-center h-full justify-center gap-4 mx-auto text-sm antialiased font-light tracking-wide pt-10">
+    <section className="bg-[url('/assets/images/ucl-global-sponsors.jpg')] bg-cover bg-center h-52 md:h-40 w-full">
+      <div className="w-4/6 md:w-full max-w-[700px] flex flex-col items-center h-full justify-center gap-4 mx-auto text-sm antialiased font-light tracking-wide pt-10">
         <p>Official global sponsors</p>
-        <div className="flex gap-4">
-          <Link href="https://www.heineken.com/" target="_blank">
-            <Image src="/assets/images/heineken.webp" alt="Heineken" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.playstation.com/" target="_blank">
-            <Image src="/assets/images/ps5.avif" alt="Play Station Five" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.lays.com/" target="_blank">
-            <Image src="/assets/images/lays.webp" alt="Lays" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.fedex.com/" target="_blank">
-            <Image src="/assets/images/fedex.webp" alt="FedEx" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.mastercard.com/" target="_blank">
-            <Image src="/assets/images/mastercard.webp" alt="Mastercard" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.takeaway.com/" target="_blank">
-            <Image src="/assets/images/jet-house.webp" alt="Jet house" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.oppo.com/" target="_blank">
-            <Image src="/assets/images/oppo.webp" alt="Oppo" width={140} height={77} quality={100} />
-          </Link>
-          <Link href="https://www.turkishairlines.com/" target="_blank">
-            <Image src="/assets/images/turkish-airlines.webp" alt="Turkish Airlines" width={140} height={77} quality={100} />
-          </Link>
+        <div className="flex flex-col md:flex-row gap-2">
+          <div className="flex gap-2 md:gap-4">
+            <Link href="https://www.heineken.com/" target="_blank">
+              <Image src="/assets/images/heineken.webp" alt="Heineken" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.playstation.com/" target="_blank">
+              <Image src="/assets/images/ps5.avif" alt="Play Station Five" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.lays.com/" target="_blank">
+              <Image src="/assets/images/lays.webp" alt="Lays" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.fedex.com/" target="_blank">
+              <Image src="/assets/images/fedex.webp" alt="FedEx" width={140} height={77} quality={100} />
+            </Link>
+          </div>
+          <div className="flex gap-2 md:gap-4">
+            <Link href="https://www.mastercard.com/" target="_blank">
+              <Image src="/assets/images/mastercard.webp" alt="Mastercard" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.takeaway.com/" target="_blank">
+              <Image src="/assets/images/jet-house.webp" alt="Jet house" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.oppo.com/" target="_blank">
+              <Image src="/assets/images/oppo.webp" alt="Oppo" width={140} height={77} quality={100} />
+            </Link>
+            <Link href="https://www.turkishairlines.com/" target="_blank">
+              <Image src="/assets/images/turkish-airlines.webp" alt="Turkish Airlines" width={140} height={77} quality={100} />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
